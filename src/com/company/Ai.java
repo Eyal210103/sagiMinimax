4fd14ac4a2b317e9bf685311e80b6e1424a31a6b
eyal darmon <eyal210103eyal@gmail.com>
2021-06-05 21:52:18 +0300

    Finish

--- src/com/company/Ai.java
+++ src/com/company/Ai.java
@@ -65,10 +65,10 @@ public class Ai {
                 for (int j = 1; j < gameManager.getLines().get(i) + 1; j++) {
                     gameManager.playTurn(i, j);
 
-                    if (boards.size() == (depth)) {
-                        boards.add(depth, new ArrayList<>());
-                    }
-                    boards.get(depth).add(gameManager.getBoardGraphics());
+//                    if (boards.size() == (depth)) {
+//                        boards.add(depth, new ArrayList<>());
+//                    }
+//                    boards.get(depth).add(gameManager.getBoardGraphics());
 
 
                     int score = minimax(gameManager, depth + 1, false,alpha,beta);
@@ -84,11 +84,11 @@ public class Ai {
             for (int i = 0; i < gameManager.getLines().size(); i++) {
                 for (int j = 1; j < gameManager.getLines().get(i) + 1; j++) {
                     gameManager.playTurn(i, j);
-
-                    if (boards.size() == (depth)) {
-                        boards.add(depth, new ArrayList<>());
-                    }
-                    boards.get(depth).add(gameManager.getBoardGraphics());
+//
+//                    if (boards.size() == (depth)) {
+//                        boards.add(depth, new ArrayList<>());
+//                    }
+//                    boards.get(depth).add(gameManager.getBoardGraphics());
 
 
 
@@ -115,109 +115,4 @@ public class Ai {
             }
         }
     }
-
-
-//    public Move playTurn(GameManager gameManager) {
-//        Move move = new Move(1, 1);
-//        GameManager toCheckOn = new GameManager(gameManager);
-//        int bestScore = Integer.MIN_VALUE;
-//        for (int i = 0; i < gameManager.getLines().length; i++) {
-//            for (int j = 1; j < gameManager.getLines()[i].getMaklot().size()+1; j++) {
-//                Move moveToTry = new Move(i, j);
-//                if (toCheckOn.getLines()[i].isEnough(j)) {
-//                    toCheckOn.playTurn(i, j);
-//                    int score = minimax2(toCheckOn, 1, false);
-//                    toCheckOn = new GameManager(gameManager);
-//                    System.out.println("Try    " + moveToTry + score);
-//                    if (bestScore <= score) {
-//                        bestScore = score;
-//                        move.amount = moveToTry.amount;
-//                        move.row = moveToTry.row;
-//                        System.out.println("Better           " + move + score);
-//                    }
-//                }
-//            }
-//        }
-//        System.out.println("CHOOSEN           " + move);
-//
-//        return move;
-//    }
-//
-//    public int minimax2(GameManager gameManager, int depth, boolean isMaximizing) {
-//        if (gameManager.isGameOver()) {
-//            if (isMaximizing) {
-//                return -100000 / depth;
-//            } else {
-//                return 100000/ depth;
-//            }
-//        }
-//        int bestScore;
-//        System.out.println("ITERATION________" + depth);
-//        if (isMaximizing) {
-//            bestScore = Integer.MIN_VALUE;
-//            for (int i = 0; i < gameManager.getLines().length; i++) {
-//                for (int j = 1; j <= gameManager.getLines()[i].getMaklot().size(); j++) {
-//                    if (gameManager.getLines()[i].isEnough(j)) {
-//                        gameManager.playTurn(i, j);
-//                        int score = minimax2(gameManager, depth+1, gameManager.getTurn()==0);
-//                        bestScore = Math.max(bestScore, score);
-//                    }
-//                }
-//            }
-//        } else {
-//            bestScore = Integer.MAX_VALUE;
-//            for (int i = 0; i < gameManager.getLines().length; i++) {
-//                for (int j = 1; j <= gameManager.getLines()[i].getMaklot().size(); j++) {
-//                    if (gameManager.getLines()[i].isEnough(j)) {
-//                        gameManager.playTurn(i, j);
-//                        int score = minimax2(gameManager, depth, gameManager.getTurn()==0);
-//                        bestScore = Math.min(bestScore, score);
-//                    }
-//                }
-//            }
-//        }
-//        return bestScore;
-//    }
-//
-//    public int minimax(GameManager gameManager, int depth, boolean isMaximizing) {
-//        if (gameManager.getWinner().equals("ai")) {
-//            return 1000 / depth;
-//        } else if (gameManager.getWinner().equals("player")) {
-//            return -1000 / depth;
-//        }
-//
-//        int bestScore;
-//        if (isMaximizing) {
-//            bestScore = Integer.MIN_VALUE;
-//            for (int i = 0; i < gameManager.getLines().length; i++) {
-//                for (int j = 1; j < gameManager.getLines()[i].getMaklot().size() + 1; j++) {
-//                    if (gameManager.getLines()[i].isEnough(j)) {
-//                        gameManager.setTurn(0);
-//                        if (gameManager.playTurn(i, j)) {
-//                            int score = minimax(gameManager, depth + 1, false);
-//                            if (score > bestScore)
-//                                bestScore = score;
-//                        }
-//                    }
-//                }
-//            }
-//        } else {
-//            bestScore = Integer.MAX_VALUE;
-//            for (int i = 0; i < gameManager.getLines().length; i++) {
-//                for (int j = 1; j < gameManager.getLines()[i].getMaklot().size() + 1; j++) {
-//                    if (gameManager.getLines()[i].isEnough(j)) {
-//                        gameManager.setTurn(1);
-//                        if (gameManager.playTurn(i, j)) {
-//                            int score = minimax(gameManager, depth + 1, true);
-//                            if (score < bestScore)
-//                                bestScore = score;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        return bestScore;
-//    }
-
 }
