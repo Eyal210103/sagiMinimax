bbaf36900c9f86d4a32b408d37021d9e42dab29a
eyal darmon <eyal210103eyal@gmail.com>
2021-06-07 17:30:26 +0300

    Finish

--- src/com/company/Ai.java
+++ src/com/company/Ai.java
@@ -9,8 +9,6 @@ public class Ai {
     public Ai() {
     }
 
-    //  ArrayList<ArrayList<String>> boards = new ArrayList<>();
-
     public Move playTurn(GameManager gameManager) {
         Move move = new Move(1, 1);
         int bestScore = Integer.MIN_VALUE;
@@ -18,26 +16,11 @@ public class Ai {
         for (int i = 0; i < gameManager.getLines().size(); i++) {
             for (int j = 1; j < gameManager.getLines().get(i) + 1; j++) {
                 GameManager boardToTry = new GameManager(gameManager);
-//                System.out.println("++++++++++++++++++++++++++++++++++" + count);
-//                System.out.println("INITIAL");
-//                boardToTry.print();
-//                count++;
-//                System.out.println();
-//                System.out.println("TRY");
                 boardToTry.playTurn(i, j);
-//                boardToTry.print();
-//                System.out.println();
                 int score = minimax(boardToTry, 0, false, Integer.MIN_VALUE, Integer.MAX_VALUE);
-
-                //     printTree();
-
-                // boards = new ArrayList<>();
-
-                System.out.println("check -> " + i + "," + j + " Score:" + score);
                 boardToTry.addNumToLine(i, j);
                 if (bestScore < score) {
                     bestScore = score;
-                    System.out.println("Better -------> " + i + "," + j + " Score:" + score);
                     move.row = i;
                     move.amount = j;
                 }
@@ -48,9 +31,6 @@ public class Ai {
 
     private int minimax(GameManager gameManager, int depth, boolean isMaximizing, int alpha, int beta) {
         if (!gameManager.getWinner().equals("none")) {
-//            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=");
-//            System.out.println(gameManager.getBoardGraphics());
-//            System.out.println("-=-=-=-=-=-=-=-=-=-=-=-=");
             if (depth == 0)
                 depth++;
             if (isMaximizing)
@@ -67,16 +47,10 @@ public class Ai {
             for (int i = 0; i < gameManager.getLines().size(); i++) {
                 for (int j = 1; j < gameManager.getLines().get(i) + 1; j++) {
                     gameManager.playTurn(i, j);
-
-//                    if (boards.size() == (depth)) {
-//                        boards.add(depth, new ArrayList<>());
-//                    }
-//                    boards.get(depth).add(gameManager.getBoardGraphics());
-
                     int score = minimax(gameManager, depth + 1, false, alpha, beta);
                     bestScore = Math.max(bestScore, score);
-
                     gameManager.addNumToLine(i, j);
+
                     alpha = Math.max(alpha, score);
                     if (beta <= alpha)
                         break;
@@ -87,16 +61,10 @@ public class Ai {
             for (int i = 0; i < gameManager.getLines().size(); i++) {
                 for (int j = 1; j < gameManager.getLines().get(i) + 1; j++) {
                     gameManager.playTurn(i, j);
-//
-//                    if (boards.size() == (depth)) {
-//                        boards.add(depth, new ArrayList<>());
-//                    }
-//                    boards.get(depth).add(gameManager.getBoardGraphics());
-
                     int score = minimax(gameManager, depth + 1, true, alpha, beta);
                     bestScore = Math.min(bestScore, score);
-
                     gameManager.addNumToLine(i, j);
+
                     beta = Math.min(beta, score);
                     if (beta <= alpha)
                         break;
@@ -105,16 +73,4 @@ public class Ai {
         }
         return bestScore;
     }
-
-
-//    private void printTree() {
-//        int count = 1;
-//        for (ArrayList<String> arr : boards) {
-//            System.out.println(count + ":=======:" + (count % 2 == 0 ? "ai" : "enemy"));
-//            count++;
-//            for (String str : arr) {
-//                System.out.println(str);
-//            }
-//        }
-//    }
 }
