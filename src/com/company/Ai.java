8584b793849aec361960a8a6a3aa2ee97cc50624
eyal darmon <eyal210103eyal@gmail.com>
2021-06-07 17:46:22 +0300

    Finish

--- src/com/company/Ai.java
+++ src/com/company/Ai.java
@@ -12,7 +12,6 @@ public class Ai {
     public Move playTurn(GameManager gameManager) {
         Move move = new Move(1, 1);
         int bestScore = Integer.MIN_VALUE;
-        //  int count = 1;
         for (int i = 0; i < gameManager.getLines().size(); i++) {
             for (int j = 1; j < gameManager.getLines().get(i) + 1; j++) {
                 GameManager boardToTry = new GameManager(gameManager);
@@ -30,33 +29,33 @@ public class Ai {
     }
 
     private int minimax(GameManager gameManager, int depth, boolean isMaximizing, int alpha, int beta) {
-        if (!gameManager.getWinner().equals("none")) {
-            if (depth == 0)
+        if (!gameManager.getWinner().equals("none")) { // אם המשחק נגמר
+            if (depth == 0) //מקרה קצה
                 depth++;
             if (isMaximizing)
-                return -1000 / depth;
+                return -1000 / depth; //להחזיר מינימום
             else
-                return 1000 / depth;
+                return 1000 / depth; //להחזיר מקסימום
         }
-        if (!gameManager.isLegalBoard()){
-            return -1000;
+        if (!gameManager.isLegalBoard()){ // אם התור התור חוקי והלוח נשאר חוקי
+            return -1000; // המצב הכי גרוע
         }
-            int bestScore;
-        if (isMaximizing) {
+        int bestScore;
+        if (isMaximizing) { // אם הבינה משחקת
             bestScore = Integer.MIN_VALUE;
-            for (int i = 0; i < gameManager.getLines().size(); i++) {
-                for (int j = 1; j < gameManager.getLines().get(i) + 1; j++) {
+            for (int i = 0; i < gameManager.getLines().size(); i++) { // מספר שורה
+                for (int j = 1; j < gameManager.getLines().get(i) + 1; j++) { // מספר המקלות אותם לוקחים
                     gameManager.playTurn(i, j);
                     int score = minimax(gameManager, depth + 1, false, alpha, beta);
                     bestScore = Math.max(bestScore, score);
-                    gameManager.addNumToLine(i, j);
+                    gameManager.addNumToLine(i, j); // לאפס למצב קודם
 
                     alpha = Math.max(alpha, score);
                     if (beta <= alpha)
                         break;
                 }
             }
-        } else {
+        } else { // אם הבינה לא משחקת
             bestScore = Integer.MAX_VALUE;
             for (int i = 0; i < gameManager.getLines().size(); i++) {
                 for (int j = 1; j < gameManager.getLines().get(i) + 1; j++) {
